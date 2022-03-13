932ca3c3e45c357a53f927df037b1087dc8f2693
Jason Wee <peichieh@gmail.com>
2022-03-13 12:36:39 +0800

    consumign-rest

---
+++ consuming-rest/src/main/java/ch/weetech/consumingrest/ServletInitializer.java
@@ -0,0 +1,13 @@
+package ch.weetech.consumingrest;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ConsumingRestApplication.class);
+	}
+
+}
