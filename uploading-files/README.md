a586b547f23544393a71dd8b42b4c3fe74b58ec7
Jason Wee <peichieh@gmail.com>
2022-03-18 13:22:01 +0800

    complete uploading-files

---
+++ uploading-files/README.md
@@ -0,0 +1,17 @@
+https://spring.io/guides/gs/uploading-files/
+
+```
+export JAVA_HOME=/usr/lib/jvm/jdk-17/
+```
+
+build
+```
+./gradlew build
+```
+
+run
+```
+./gradlew bootRun
+or
+/usr/lib/jvm/jdk-17/bin/java -jar build/libs/uploading-files-0.0.1-SNAPSHOT.jar
+```
---
+++ uploading-files/src/main/java/ch/weetech/uploadingfiles/UploadingFilesApplication.java
@@ -0,0 +1,28 @@
+package ch.weetech.uploadingfiles;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+import ch.weetech.uploadingfiles.storage.StorageProperties;
+import ch.weetech.uploadingfiles.storage.StorageService;
+
+@SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
+public class UploadingFilesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UploadingFilesApplication.class, args);
+	}
+
+		@Bean
+	CommandLineRunner init(StorageService storageService) {
+		return (args) -> {
+			storageService.deleteAll();
+			storageService.init();
+		};
+	}
+
+}
