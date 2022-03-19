0391e8cf124f14ecf35dd23dde8099ebe4214907
Jason Wee <peichieh@gmail.com>
2022-03-19 12:30:45 +0800

    complete messaging-redis

---
+++ messaging-redis/src/test/java/ch/weetech/messagingredis/MessagingRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.weetech.messagingredis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessagingRedisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
