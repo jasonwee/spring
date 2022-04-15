822f581d2417ced984e28d495048944268a0414f
Jason Wee <peichieh@gmail.com>
2022-04-15 13:12:07 +0800

    change to localhost greeting

--- consuming-rest-angularjs/public/hello.js
+++ consuming-rest-angularjs/public/hello.js
@@ -1,6 +1,6 @@
 angular.module('demo', [])
 .controller('Hello', function($scope, $http) {
-    $http.get('http://rest-service.guides.spring.io/greeting').
+    $http.get('http://localhost:8080/greeting').
         then(function(response) {
             $scope.greeting = response.data;
         });
